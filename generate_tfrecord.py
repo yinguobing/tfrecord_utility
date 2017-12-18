4b6540880a1f4e3708df12dce414f58c0d2c19ee
yinguobing <yinguobing@gmail.com>
2017-12-18 19:15:44 +0800

    add function for float type feature.

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -36,6 +36,10 @@ def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
+def _float32_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 

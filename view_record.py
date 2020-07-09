ad6c879d4764216c833178551365afe730189c39
yinguobing <yinguobing@gmail.com>
2020-07-09 16:34:35 +0800

    added function to decompose the record file

--- view_record.py
+++ view_record.py
@@ -5,6 +5,8 @@ Usage:
 import argparse
 
 import cv2
+import os
+import json
 import numpy as np
 import tensorflow as tf
 
@@ -16,6 +18,8 @@ MARK_SIZE = 68 * 2
 parser = argparse.ArgumentParser()
 parser.add_argument("--record", type=str, default="train.record",
                     help="The record file.")
+parser.add_argument("--save_dir", type=str, default=None,
+                    help="The directory where the files will be saved.")
 args = parser.parse_args()
 
 
@@ -56,28 +60,48 @@ def show_record(filenames):
     parsed_dataset = parse_tfrecord(filenames)
 
     for example in parsed_dataset:
-        image_decoded = tf.image.decode_image(example['image/encoded']).numpy()
-        height = example['image/height'].numpy()
-        width = example['image/width'].numpy()
-        depth = example['image/depth'].numpy()
-        filename = example['image/filename'].numpy()
-        img_format = example['image/format'].numpy()
+        filename = example['image/filename'].numpy().decode("utf-8")
+        img_format = example['image/format'].numpy().decode("utf-8")
         marks = example['label/marks'].numpy()
 
-        print(filename, img_format, width, height, depth)
+        if args.save_dir:
+            # Write the images.
+            image_path = os.path.join(args.save_dir, "image")
+            if not os.path.exists(image_path):
+                os.makedirs(image_path)
+            image_file_name = os.path.join(
+                image_path, filename + "." + img_format)
+            with tf.io.gfile.GFile(image_file_name, 'wb') as fid:
+                fid.write(example['image/encoded'].numpy())
 
-        # Use OpenCV to preview the image.
-        image = np.array(image_decoded, np.uint8)
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            # Write the marks.
+            mark_path = os.path.join(args.save_dir, "mark")
+            if not os.path.exists(mark_path):
+                os.makedirs(mark_path)
+            mark_file = os.path.join(mark_path, filename + ".json")
+            with open(mark_file, 'w') as fid:
+                json.dump(marks.tolist(), fid)
+        else:
+            image_decoded = tf.image.decode_image(
+                example['image/encoded']).numpy()
+            height = example['image/height'].numpy()
+            width = example['image/width'].numpy()
+            depth = example['image/depth'].numpy()
 
-        # Draw the landmark on image
-        landmark = np.reshape(marks, (-1, 2)) * IMG_SIZE
-        _draw_landmark_point(image, landmark)
+            print(filename, img_format, width, height, depth)
 
-        # Show the result
-        cv2.imshow("image", image)
-        if cv2.waitKey() == 27:
-            break
+            # Use OpenCV to preview the image.
+            image = np.array(image_decoded, np.uint8)
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            # Draw the landmark on image
+            landmark = np.reshape(marks, (-1, 2)) * IMG_SIZE
+            _draw_landmark_point(image, landmark)
+
+            # Show the result
+            cv2.imshow("image", image)
+            if cv2.waitKey() == 27:
+                break
 
 
 if __name__ == "__main__":
